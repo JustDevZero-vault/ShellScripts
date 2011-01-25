15393bbab283bf122e81052d6b051da59ea11260
Mephiston Snake <meph.snake@gmail.com>
2011-01-25 14:14:12 +0100

    Some changes

--- massuploader.zsh
+++ massuploader.zsh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+#       massuploadero.py
+#       
+#       Copyright 2011 Mephiston Snake <meph dot snake at gmail dot com>
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 3 of the License, or
